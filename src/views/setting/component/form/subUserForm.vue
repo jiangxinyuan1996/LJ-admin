7b1612abe1bf92953bd2cd24ef3e9f8c05f575b4
unknown <zhengxy@allinpay.corp>
2020-06-11 14:01:38 +0800

    服务商配置也进行调整,已经创建的服务商信息不可进行修改.完善了新的服务商导入模板

--- src/views/setting/component/form/subUserForm.vue
+++ src/views/setting/component/form/subUserForm.vue
@@ -15,23 +15,23 @@
         </el-select>
       </el-form-item>
       <el-form-item label="账户名" prop="name" style="margin-top:0;display: inline-block;">
-        <el-input v-model="createForm.name" style="width:15vw;" />
+        <el-input v-model="createForm.name" :disabled="state=='update'" style="width:15vw;" />
       </el-form-item>
       <el-form-item label="联系方式" prop="phone">
-        <el-input v-model="createForm.phone" style="width:15vw;" />
+        <el-input v-model="createForm.phone" :disabled="state=='update'" style="width:15vw;" />
       </el-form-item>
       <el-form-item label="身份证" prop="id">
-        <el-input v-model="createForm.id" style="width:15vw;" />
+        <el-input v-model="createForm.id" :disabled="state=='update'" style="width:15vw;" />
       </el-form-item>
       <el-form-item label="银行卡号" prop="card_no">
-        <el-input v-model="createForm.card_no" style="width:15vw;" />
+        <el-input v-model="createForm.card_no" :disabled="state=='update'" style="width:15vw;" />
       </el-form-item>
       <el-form-item label="预留手机号" prop="tel">
-        <el-input v-model="createForm.tel" style="width:15vw;" />
+        <el-input v-model="createForm.tel" :disabled="state=='update'" style="width:15vw;" />
       </el-form-item>
       <el-divider />
       <el-form-item label="银行名称" prop="bank" style="margin-top:0;display: inline-block;">
-        <el-select v-model="createForm.bank" filterable clearable placeholder="请选择">
+        <el-select v-model="createForm.bank" :disabled="state=='update'" filterable clearable placeholder="请选择">
           <el-option
             v-for="item in bankCodeOptions"
             :key="item.key"
@@ -41,18 +41,18 @@
         </el-select>
       </el-form-item>
       <el-form-item label="企业名称" prop="company_name">
-        <el-input v-model="createForm.company_name" style="width:15vw;" />
+        <el-input v-model="createForm.company_name" :disabled="state=='update'" style="width:15vw;" />
       </el-form-item>
       <el-form-item label="统一社会信用" prop="company_id">
-        <el-input v-model="createForm.company_id" style="width:15vw;" />
+        <el-input v-model="createForm.company_id" :disabled="state=='update'" style="width:15vw;" />
       </el-form-item>
       <el-form-item label="开户行支行" prop="subbranch">
-        <el-input v-model="createForm.subbranch" style="width:15vw;" />
+        <el-input v-model="createForm.subbranch" :disabled="state=='update'" style="width:15vw;" />
       </el-form-item>
       <el-form-item label="支行行号" prop="subbranch_num">
-        <el-input v-model="createForm.subbranch_num" style="width:15vw;" />
+        <el-input v-model="createForm.subbranch_num" :disabled="state=='update'" style="width:15vw;" />
       </el-form-item>
-      <el-form-item label="营业执照" prop="business_license">
+      <el-form-item label="营业执照" v-if="state=='create'" prop="business_license">
         <el-upload
           ref="upload"
           class="upload-demo"
@@ -67,7 +67,7 @@
           <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
         </el-upload>
       </el-form-item>
-      <el-form-item label="开户许可证" prop="account_opening_permit">
+      <el-form-item label="开户许可证" v-if="state=='create'"prop="account_opening_permit">
         <el-upload
           ref="upload"
           class="upload-demo"
@@ -82,7 +82,7 @@
           <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
         </el-upload>
       </el-form-item>
-      <el-form-item label="身份证(正)" prop="id_card_f">
+      <el-form-item label="身份证(正)" v-if="state=='create'"prop="id_card_f">
         <el-upload
           ref="upload"
           class="upload-demo"
@@ -97,7 +97,7 @@
           <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
         </el-upload>
       </el-form-item>
-      <el-form-item label="身份证(反)" prop="id_card_b">
+      <el-form-item label="身份证(反)" v-if="state=='create'"prop="id_card_b">
         <el-upload
           ref="upload"
           class="upload-demo"
